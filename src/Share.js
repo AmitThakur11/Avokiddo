c2732556e867a62261328f424c26a5d83d73089e
Amit Thakur <amithakur081@gmail.com>
2024-12-03 12:49:28 +0000

    buttons added

--- src/Share.js
+++ src/Share.js
@@ -1,7 +1,18 @@
+import AppStore from "./assets/app-store.svg";
+import PlayStore from "./assets/play-store.png";
 export const Share = () => {
   return (
     <div>
-      <p>Avokiddo</p>
+      <h1>Get Avokiddo</h1>
+      <div>
+        <a href="itms-apps://play.google.com/store/apps">
+          <img src={AppStore} alt="apple" />
+        </a>
+
+        <a href="market://details?id=com.android.vending">
+          <img src={PlayStore} alt="google" />
+        </a>
+      </div>
     </div>
   );
 };
