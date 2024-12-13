850658f839749312f3462f0e54ba43c1a273f91f
Amit Thakur <amithakur081@gmail.com>
2024-12-13 07:16:48 +0000

    new changes

--- src/Share.js
+++ src/Share.js
@@ -1,17 +1,34 @@
 import AppStore from "./assets/app-store.svg";
 import PlayStore from "./assets/play-store.png";
+import Avokiddo from "./assets/avokiddo-logo.png";
+import { useState, useEffect } from "react";
 export const Share = () => {
+  const [inviteData, setInviteData] = useState({
+    platform: "",
+  });
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const platform = url.searchParams.get("platform") ?? "all";
+    console.log("platform---->", platform);
+    setInviteData({
+      platform: platform,
+    });
+  }, []);
   return (
     <div>
-      <h1>Get Avokiddo</h1>
+      <h1>Download Avokiddo</h1>
       <div>
-        <a href="itms-apps://play.google.com/store/apps">
-          <img src={AppStore} alt="apple" />
-        </a>
-
-        <a href="market://details?id=com.android.vending">
-          <img src={PlayStore} alt="google" />
-        </a>
+        {(inviteData.platform === "ios" || inviteData.platform === "all") && (
+          <a href="itms-apps://play.google.com/store/apps">
+            <img src={AppStore} alt="apple" />
+          </a>
+        )}
+        {(inviteData.platform === "android" ||
+          inviteData.platform === "all") && (
+          <a href="market://details?id=com.android.vending">
+            <img src={PlayStore} alt="google" />
+          </a>
+        )}
       </div>
     </div>
   );
